b5e12e550d8b5dec0bccd9c008bf9edff1a7bdc7
msuhail1997 <36642743+msuhail1997@users.noreply.github.com>
2020-04-23 23:27:18 -0400

    Add files via upload

---
+++ mypath.py
@@ -0,0 +1,16 @@
+from util.path_abstract import PathAbstract
+
+
+class Path(PathAbstract):
+    @staticmethod
+    def db_root_dir():
+        return '/content/drive/My Drive/DAVIS 2016/DAVIS/'
+
+    @staticmethod
+    def save_root_dir():
+        return '/content/OSVOS-PyTorch/models'
+
+    @staticmethod
+    def models_dir():
+        return "/content/OSVOS-PyTorch/models"
+
